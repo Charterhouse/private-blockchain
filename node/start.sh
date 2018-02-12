ca6619fa0a8e56dd29285c9377c4bd80b4acbaaa
Marcin Czenko <marcin@everydayproductive.com>
2018-02-12 13:16:46 +0100

    Added --rpccorsdomain "*" for our regular nodes and updated the nodes-stack to use the updated nodes docker image.

--- node/start.sh
+++ node/start.sh
@@ -22,6 +22,6 @@ geth --datadir /node/data \
      --networkid 5 \
      --gasprice "20000000000" \
      --targetgaslimit "4712388" \
-     --rpc --rpcport 8545 --rpcaddr "0.0.0.0" --rpcapi "db,eth,net,web3,personal,web3" \
+     --rpc --rpcport 8545 --rpcaddr "0.0.0.0" --rpcapi "db,eth,net,web3,personal,web3" --rpccorsdomain "*" \
      --port 30303 $BOOTNODES \
      2>>/node/blockchain.log
